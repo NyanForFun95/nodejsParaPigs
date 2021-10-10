d400ef81856f19c39432d6e9f2d3a97a8f98f270
Johann Flores <johannflores.n29@gmail.com>
2021-10-10 12:31:43 -0500

    ronda de ejercicios 3

--- ejercicios/ejercicios3/ejercicio10.js
+++ ejercicios/ejercicios3/ejercicio10.js
@@ -1,1 +1,2 @@
 // Escribe una función que te genere un array que tenga los primeros n números de Fibonacci. Genera un array con la cantidad de divisores de los primeros 100 números de Fibonacci.
+
--- ejercicios/ejercicios3/ejercicio9.js
+++ ejercicios/ejercicios3/ejercicio9.js
@@ -1,1 +1,5 @@
 // Escribe una función que te regrese el factorial de numero n. Imprime los números 12!, 21!, 13!+17!
+
+function factorial(number)
+let factorial = 1;
+for(let a = 1; )
