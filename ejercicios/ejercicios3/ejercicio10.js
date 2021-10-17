e9b35876d1265858901fe3ca89b08f53547cf760
Johann Flores <johannflores.n29@gmail.com>
2021-10-17 16:32:59 -0500

    ejercicios

--- ejercicios/ejercicios3/ejercicio10.js
+++ ejercicios/ejercicios3/ejercicio10.js
@@ -1,2 +1,28 @@
 // Escribe una función que te genere un array que tenga los primeros n números de Fibonacci. Genera un array con la cantidad de divisores de los primeros 100 números de Fibonacci.
 
+//function fibonacci(number){;
+//   let fibonacci = 0;
+//   for (let a = 0; a<=number; a++){;
+//       fibonacci + (fibonacci)
+//    }; 
+//    return fibonacci;
+    
+//};
+
+
+function divisores (length) {
+   let aux = length;
+   while (aux + aux  >100){
+      length++;
+      aux = 1;
+   };
+   return length;
+};
+
+var fibonacci = [1,1];
+
+while(divisores(fibonacci[fibonacci.length-1]+fibonacci[fibonacci.length-2])<100){
+   fibonacci.push(fibonacci[fibonacci.length-1]+fibonacci[fibonacci.length-2]);
+};
+
+console.log(fibonacci.length)
