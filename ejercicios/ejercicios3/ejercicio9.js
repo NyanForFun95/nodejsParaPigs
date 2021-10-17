40ef19a0b48f68601b3f36b65c6f01385cc0493c
Johann Flores <johannflores.n29@gmail.com>
2021-10-17 16:37:43 -0500

    commit

--- ejercicios/ejercicios3/ejercicio9.js
+++ ejercicios/ejercicios3/ejercicio9.js
@@ -1,5 +1,13 @@
 // Escribe una función que te regrese el factorial de numero n. Imprime los números 12!, 21!, 13!+17!
 
-function factorial(number)
-let factorial = 1;
-for(let a = 1; )
+function factorial(number){
+    let factorial = 1;
+    for(let a = 1; a<=number; a++){
+        factorial = factorial * a;  //*=
+    };
+    return factorial; 
+};
+
+console.log(factorial(12));
+console.log(factorial(21));
+console.log(factorial(13)+factorial(17));
